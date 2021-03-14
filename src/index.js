c50b0382f80d1f2bd576b18fb414c5ece109733b
UncleScrooge2012 <scrooge2024@yandex.ru>
2021-03-14 18:15:57 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0 ) {
+        let str = String(n * -1 );
+        let newStr = ''
+        for(let i = str.length - 1; i >= 0 ; i--) {
+            newStr += str[i]
+          }
+          return Number(newStr)
+
+    }
+
+  let str = String(n)
+  let newStr = ''
+  for(let i = str.length - 1; i >= 0 ; i--) {
+    newStr += str[i]
+  }
+  return Number(newStr)
 }
+ 
